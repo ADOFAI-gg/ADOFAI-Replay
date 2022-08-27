063d6428ed7b1a1e6e214937e851775bc61866ae
FLOWER <pdgyu7777@gmail.com>
2022-08-27 19:15:57 +0900

    chore: server upload async

--- Replay/Functions/Saving/SaveReplay.cs
+++ Replay/Functions/Saving/SaveReplay.cs
@@ -3,21 +3,22 @@ using System.Collections;
 using System.IO;
 using System.Net;
 using System.Text;
+using Replay.Functions.Core;
 using UnityEngine;
 
 namespace Replay.Functions.Saving
 {
     public class SaveReplay
     {
-        public const string TEST_URL = test_server_ip;
-        public const string SERVER_URL = private_server_ip;
+        public const string TEST_URL = ServerManager.TEST_URL;
+        public const string SERVER_URL = ServerManager.SERVER_URL;
 
         public static void UploadToServer(string data)
         {
             var wc = new WebClient();
             wc.Headers[HttpRequestHeader.ContentType] = "application/json";
             wc.Encoding = Encoding.UTF8;
-            wc.UploadStringAsync(new Uri(Replay.IsDebug ? TEST_URL : SERVER_URL),
+            wc.UploadString(new Uri(Replay.IsDebug ? TEST_URL : SERVER_URL),
                 "{\"auth\": \"여기 서버에다가 테러 비스무리한거 하면은 IP밴 때리고 IP추적해서 빠따날립니다.\", \"rpl\":" + data + " }");
         }
         
